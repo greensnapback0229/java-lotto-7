cabb2d3ebe61a6aeb21ae59ec00087cae75137bd
greensnapback0229 <smdmim@gmail.com>
2024-11-04 23:40:47 +0900

    feat($lotto): add buying lotto

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,28 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
+
+public class LottoService {
+
+    public List<Lotto> buyLotto(Integer purchasePrice) {
+        Integer countToBuy = purchasePrice / 1000;
+
+        List<Lotto> lottoList = new ArrayList<>();
+
+        for (int count = 0; count < countToBuy; count++) {
+            lottoList.add(makeNewLotto());
+        }
+
+        return lottoList;
+    }
+
+
+    private Lotto makeNewLotto() {
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+
+        return new Lotto(lottoNumbers);
+    }
+}
