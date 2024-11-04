dba683224db5431871e1adf18c6e725909bcd4b4
greensnapback0229 <smdmim@gmail.com>
2024-11-04 19:10:38 +0900

    test(InputTest): add wrong type test and test exception message

--- src/test/java/lotto/viewTest/InputTest.java
+++ src/test/java/lotto/viewTest/InputTest.java
@@ -1,7 +1,9 @@
 package lotto.viewTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 import java.io.ByteArrayInputStream;
 import lotto.view.Input;
@@ -29,7 +31,7 @@ public class InputTest {
 
     @Test
     @DisplayName("1000원 단위로 입력이 아닐때")
-    public void testPurchasePriceWrong() {
+    public void testPurchasePriceNotInThousands() {
 
         //given
         Integer wrongPrice = 13100;
@@ -38,7 +40,23 @@ public class InputTest {
         input.inputPurchasePrice();
 
         //then
-        assertThrows(IllegalArgumentException.class, () -> systemInputString(wrongPrice.toString()));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> systemInputString(wrongPrice.toString()));
+        assertThat(exception.getMessage()).isEqualTo("[ERROR] 1000원 단위로만 구매 가능합니다");
+    }
+    
+    @Test
+    @DisplayName("정수형 입력이 아닐때")
+    public void testPurchasePriceNotInteger() {
+
+        //given
+        String wrongNumber = "1l230O0Oo"; //O는 알파벳 0은 숫자
+
+        //when
+        input.inputPurchasePrice();
+
+        //then
+        IllegalArgumentException exception =  assertThrows(IllegalArgumentException.class, () -> systemInputString(wrongNumber));
+        assertThat(exception.getMessage()).isEqualTo("[ERROR] 숫자만 입력해주세요");
     }
 
     public static void systemInputString(String s){
